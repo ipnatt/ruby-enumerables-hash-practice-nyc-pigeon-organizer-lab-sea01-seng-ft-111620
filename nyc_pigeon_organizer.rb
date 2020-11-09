dad9184490f96cb728292032e1713d5ebc57a8cd
Irvin Natt <ipnatt@gmail.com>
2020-11-09 00:48:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,15 @@
 def nyc_pigeon_organizer(data)
-  hash.each_with_object({}) do |(keys, values), a|
-    a[k] = v + 3
+  final_results = data.each_with_object ({}) do |(key, value), final_array|
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !final_array[name]
+          final_array[name] = {}
+        end
+        if !final_array[name][key]
+          !final_array[name][key] = []
+        end
+        final_array[name][key].push(inner_key.to_s)
+      end
+    end
   end
 end
